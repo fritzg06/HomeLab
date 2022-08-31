a9ae90123a01f198602dc5a689b82d89667cc3cc
Fritz Reyes <35794702+fritzg06@users.noreply.github.com>
2022-09-01 07:02:17 +0800

    Create MaconomyServicesEventLogsCheck.ps1

---
+++ Maconomy Services Check/MaconomyServicesEventLogsCheck.ps1
@@ -0,0 +1,5 @@
+﻿# Windows Event Logs - Coupling Service
+Get-EventLog -LogName System | Where-Object {$_.Message -like "*Coupling Service*"} | Select TimeGenerated, EventID, Message -First 10 | ft -auto
+
+# Windows Event Logs - People Planner Service
+Get-EventLog -LogName System | Where-Object {$_.Message -like "*People Planner*"} | Select TimeGenerated, EventID, Message -First 10 | ft -auto
